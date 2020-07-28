0f896bc921aacfbf74ebd904f979bee0c45999ab
zawar <zawar92>
2020-07-29 01:06:46 +0200

    postgress queries working

--- src/component-api.ts
+++ src/component-api.ts
@@ -1,7 +1,9 @@
 import Router from "koa-router";
 import path from "path";
 import {Json} from "sequelize/types/lib/utils";
-// const pool = require('../database/postgres-db');
+import * as util from "util";
+import {callbackify} from "util";
+const pool = require('../database/postgres-db');
 
 const koaBody = require("koa-body")({multipart:true});
 const koaStatic = require('koa-static')
@@ -77,49 +79,40 @@ router.post("/upload",koaBody ,async (ctx, next) => {
     var ext_name = require('path');
     var ext = path.extname(file_name.toString());
 
-    function init(__extpath: string) {
-        if (fs.existsSync(__extpath)) {
-            return JSON.parse(fs.readFileSync(path.join(__extpath,fs.readdirSync(__extpath).filter((name:string) => path.extname(name) === '.json')[0])));// Do something
-        }
-        else{
-            return 'Path '+__extpath+' Does Not Exsist';
-        }
-    }
 
     if(ext == '.tar') {
         if (!file_name) {
-            console.log("There was an error")
             ctx.response.redirect("/");
         } else {
-            console.log('inside',counter);
             const reader = fs.createReadStream(file_path);
 
             const updated_filename = counter.toString() + ".tar";
 
             const stream = fs.createWriteStream(path.join("static/uploads/", updated_filename));
-            const target = path.join(__Extractdirname,updated_filename.toString());
-            const writeStream = tar.extract(target.toString());
+            // const target = path.join(__Extractdirname,updated_filename.toString());
+            // const writeStream = tar.extract(target.toString());
             reader.pipe(stream);
-            reader.pipe(writeStream);
+            // reader.pipe(writeStream);
 
-            writeStream.on('entry', function(data: { path: any; value: any; }, stream: any, callback: any) {
-                var datas = JSON.stringify(stream.toString());
-                // var chunk = datas.substring(1,datas.length-1);
-                console.log('path:', datas);
+            reader.on('data', (chunk: any) => {
+                var a = chunk.toString().match(/{([^}]*)}/);
+                var b = JSON.parse(a[0]);
+                pool.connect();
+                var QS1 = util.format("INSERT INTO component(uuid, name, provider, description, path, version_id)VALUES('%s','%s','%s','%s','%s','2')", b.id, b.name, b.provider, b.description, updated_filename.toString());
+                pool.query(QS1,
+                    (err: any, res: any) => {
+                        console.log('here1',err, res);
+                    }
+                );
+                var QS2 = util.format("INSERT INTO component_version(version_id, version_name, version_path)VALUES('2','%s','%s')", b.version, updated_filename);
+                pool.query(QS2,
+                    (err: any, res: any) => {
+                        console.log('here2',err, res);
+                        // pool.end();
+                    }
+                );
             });
-            // pool.connect();
-            // pool.query(
-            //     "INSERT INTO component(uuid, name, provider, description, path, version_id)VALUES(1, 'Wilters', 'walter', '74 S Westgate St', 'mroyster@royster.com',1)",
-            //     (err: any, res: any) => {
-            //         console.log(err, res);
-            //     }
-            // );
-            // pool.query("INSERT INTO component_version(version_id, version_name, version_path)VALUES(1, '1', 'mroyster@royster.com')",
-            //     (err: any, res: any) => {
-            //         console.log(err, res);
-            //         pool.end();
-            //     }
-            // );
+
             console.log('uploading %s -> %s', file_name, stream.path);
             list_of_components.push(updated_filename);
             ctx.response.redirect('/components/'+ counter);
@@ -133,7 +126,41 @@ router.post("/upload",koaBody ,async (ctx, next) => {
         ctx.res.statusCode = 422;
     }
 });
+// update query
+// get component query
+function updatecomponents(file_name: any) {
+    return new Promise((resolve, reject)=> {
+        pool.connect();
+        pool.query("select c.uuid, c.name, c.provider, c.description, c.path, c.version_id, cv.version_name from component c inner join component_version cv on c.version_id=cv.version_id where path='" + file_name + "'",
+            function(err: any, data: unknown) {
+                if(err) {
+                    return reject(err);
+                }
+                var infer = JSON.parse(JSON.stringify(data));
+                if (infer.rows.length == 0){
+                    resolve("File not Present");
+                }
+                else{
+                    var up = infer.rows[0];
+                    pool.query("update component set uuid='" + up.uuid + "', name='" + up.name + "', provider='" + up.provider + "', description='" + up.description + "', path='" + file_name + "', version_id='" + up.version_id + "' where path='" + file_name + "'",
+                        function(err: any, data: unknown) {
+                            if(err) {
+                                return reject(err);
+                            }
+                            resolve("Component updated");
+                        });
 
+                    pool.query("update component_version set version_id='" + up.version_id + "', version_name='" + up.version_name + "', version_path='" + file_name + "' where path='" + file_name + "'",
+                        function(err: any, data: unknown) {
+                            if(err) {
+                                return reject(err);
+                            }
+                            resolve("Component version updated");
+                        });
+                }
+            });
+    })
+}
 // Update File to server API
 router.put("/update", koaBody,async ctx => {
     const file_name = ctx.request.body.files.foo.name;
@@ -146,10 +173,12 @@ router.put("/update", koaBody,async ctx => {
             console.log("There was an error")
             ctx.response.redirect("/");
         } else {
+            const users = await updatecomponents(file_name);
+            console.log(users);
             const reader = fs.createReadStream(file_path);
             const stream = fs.createWriteStream(path.join("static/uploads/", counter.toString())+".tar");
             reader.pipe(stream);
-            console.log('uploading %s -> %s', file_name, stream.path);
+            // console.log('uploading %s -> %s', file_name, stream.path);
             ctx.response.redirect('/components/'+ counter);
             ctx.response.header;
             ctx.res.statusCode = 201;
@@ -162,7 +191,20 @@ router.put("/update", koaBody,async ctx => {
     }
 });
 
-
+// delete component query
+function deletecomponents(file_name: any) {
+    return new Promise((resolve, reject)=> {
+        pool.connect();
+        pool.query("delete from component where path='" + file_name + "'",
+            function(err: any, data: unknown) {
+                if(err) {
+                    return reject(err);
+                }
+                var infer = JSON.parse(JSON.stringify(data));
+                resolve(infer.rows);
+            });
+    })
+}
 // Remmove api
 router.delete("/delete", koaBody,async ctx => {
     const file_name = ctx.request.query.file;
@@ -181,8 +223,10 @@ router.delete("/delete", koaBody,async ctx => {
                 console.log('File deleted!');
                 ctx.body = "File have been removed";
                 ctx.res.statusCode = 200;
+
             }
         });
+        await deletecomponents(file_name);
     }
     else{
         ctx.body = "Wrong Extension."
@@ -191,28 +235,52 @@ router.delete("/delete", koaBody,async ctx => {
 
 });
 
+// get component query
+function getcomponents() {
+    return new Promise((resolve, reject)=> {
+        pool.connect();
+        pool.query("select path from component",
+            function(err: any, data: unknown) {
+                if(err) {
+                    return reject(err);
+                }
+                var infer = JSON.parse(JSON.stringify(data));
+                resolve(infer.rows);
+            });
+    })
+}
+
 //List All Components
 router.get("/list_components", async (ctx, next) => {
-    ctx.body = {'Components_List' : list_of_components};
+    const users = await getcomponents();
+    ctx.body = users;
+        // {'Components_List' : list_of_components};
     ctx.res.statusCode = 200;
 });
 
-
+// get file data component query
+function getdata(file_name: string) {
+    return new Promise((resolve, reject)=> {
+        pool.connect();
+        pool.query("select * from component where path='" + file_name + "'",
+            function(err: any, data: unknown) {
+                if(err) {
+                    return reject(err);
+                }
+                var infer = JSON.parse(JSON.stringify(data));
+                resolve(infer.rows[0]);
+            });
+    })
+}
 
 // View Details
 router.get("/view_details", async (ctx, next) => {
     var file_name = ctx.request.query.file;
     var ext = path.extname(file_name.toString());
-    const __extpath = path.join(__Extractdirname,file_name.toString(),'sample_component');
-    var source = path.join(__Uploaddirname,file_name);
-    var target = path.join(__Extractdirname,file_name);
+    // const __extpath = path.join(__Extractdirname,file_name.toString(),'sample_component');
+    // var source = path.join(__Uploaddirname,file_name);
+    // var target = path.join(__Extractdirname,file_name);
     var someVal = list_of_components.includes(file_name);
-    console.log('SomeVal:',someVal,'Filename:',file_name,'Source:',source,'Target:',target);
-
-
-    function init() {
-        return JSON.parse(fs.readFileSync(path.join(__extpath,fs.readdirSync(__extpath).filter((name:string) => path.extname(name) === '.json')[0])));
-    }
 
     if(ext == '.tar'){
         if (!file_name) {
@@ -221,19 +289,10 @@ router.get("/view_details", async (ctx, next) => {
         else{
             if(someVal == true){
                 // ctx.body = await packTar(source,target);
-                var ret = await packTar(source, target);
-                if( ret == 1)
-                {
-                    // pool.connect();
-                    // let query = 'select * from component';
-                    // pool.query(query, function (err: any, result: any) {
-                    //     if (err) throw new Error(err);
-                    //     console.log(init());
-                    //     // var obj = JSON.parse(text);
-                    //     ctx.body = result.rows;
-                    //     ctx.res.statusCode = 200;
-                    // });
-                }
+                // var ret = await packTar(source, target);
+                const users = await getdata(file_name);
+                ctx.body = users;
+                ctx.res.statusCode = 200;
             }
             else{
                 ctx.body = "File Not Present";
